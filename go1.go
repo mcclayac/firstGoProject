0376ceac9b4fc468f66448733764d5e325cab85c
mcclayac <anthony.mcclay@me.com>
2018-12-03 19:45:52 -0600

    Declare variables and constants

--- go1.go
+++ go1.go
@@ -5,7 +5,29 @@ import (
 	"time"
 )
 
-var carTypeDist1 string = "Toyota"
+//var carTypeDist1 string = "Toyota"
+//var carTypeDist2 = "Ford"
+
+//var carTypeDist1, carTypeDist2  string = "Toyota", "Ford"
+
+//var carTypeDist1, carTypeDist2 = "Toyota", "Ford"
+
+var (
+	carTypeDist1 = "Toyota"
+	carTypeDist2 = "Ford"
+)
+
+//const toyotaHeadQuarters  = "Toyota Motor Corporation"
+//const fordHeadQuarters  = "Ford Motor Company "
+
+//const (
+//	toyotaHeadQuarters  = "Toyota Motor Corporation"
+//	fordHeadQuarters  = "Ford Motor Company "
+//)
+
+const toyotaHeadQuarters, fordHeadQuarters = "Toyota Motor Corporation", "Ford Motor Company "
+
+//var newHQ string = "NewHQ"
 
 func main() {
 	fmt.Println("Hello Tony, these nuts, 世界")
@@ -16,5 +38,16 @@ func main() {
 	//-------------------------------------------------
 
 	fmt.Println(carTypeDist1)
+	fmt.Println(carTypeDist2)
 
+	//-------------------------------------------------
+	//toyotaHeadQuarters = "Ford HQ"   error cannot assign const
+
+	newHQ := "Tony's Home" // In-line assignment
+	const myConst = "No Value"
+
+	fmt.Println(toyotaHeadQuarters)
+	fmt.Println(fordHeadQuarters)
+	fmt.Println(newHQ, myConst)
+	//fmt.Println(myConst)
 }
