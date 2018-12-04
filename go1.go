cd3132f773e329a291135624cfe173c9d4aaf395
mcclayac <anthony.mcclay@me.com>
2018-12-03 19:20:06 -0600

    Initial commit

---
+++ go1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var carTypeDist1 string = "Toyota"
+
+func main() {
+	fmt.Println("Hello Tony, these nuts, 世界")
+	fmt.Println("This is the Golong")
+	fmt.Printf("2 + 2 = %v", 2+2)
+	fmt.Println(time.Now())
+
+	//-------------------------------------------------
+
+	fmt.Println(carTypeDist1)
+
+}
